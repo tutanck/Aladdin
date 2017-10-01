a3524d4b32218517d75dcfdcdb1c1fa9511c542d
tutanck <tutanck@gmail.com>
2017-10-01 06:51:42 +0200

    img profile

--- app/src/main/java/com/aj/aladdin/tools/components/fragments/ImageFragment.java
+++ app/src/main/java/com/aj/aladdin/tools/components/fragments/ImageFragment.java
@@ -1,7 +1,11 @@
 package com.aj.aladdin.tools.components.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +14,18 @@ import android.widget.ImageView;
 
 import com.aj.aladdin.R;
 
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class ImageFragment extends Fragment {
+    public static final int PICK_IMAGE_REQUEST = 1;
 
     private static final String KEY = "KEY";
     private static final String EDITABLE = "EDITABLE";
+
+    private ImageView imageView;
 
     public static ImageFragment newInstance(
             String key
@@ -37,17 +48,41 @@ public class ImageFragment extends Fragment {
     ) {
         final Bundle args = getArguments();
 
-        ImageView imageView = (ImageView) inflater.inflate(R.layout.fragment_image_view, container, false);
+        imageView = (ImageView) inflater.inflate(R.layout.fragment_image_view, container, false);
 
         if (args.getBoolean(EDITABLE))
             imageView.setOnClickListener(
                     new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-
+                            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                            intent.setType("image/*");
+                            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
                         }
                     }
             );
         return imageView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }
